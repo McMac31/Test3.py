991129bc1cc1a7c11dce6ed77d5dd71b4833ada1
Miguel Campo <125380860+McMac31@users.noreply.github.com>
2023-02-14 17:53:34 +0100

    Update and rename README.md to Activity2.py

---
+++ Activity2.py
@@ -0,0 +1,29 @@
+# #Programa para imprimir tu nombre
+
+# nombre = input("¿Cúal es tu nombre?")
+# for i in range(50):
+#     print(f"{nombre}")
+
+#Programa para contar
+
+# accion = input("Teclea Up para contar en positivo desde 0/ teclea Down para contar en negativo")
+# numeros_de_veces = int(input("¿Cuantas veces quieres repetir?"))
+# if accion == "Up" or accion == "up":
+#     for i in range(numeros_de_veces):
+#         print(i)
+# elif accion == "Down" or accion == "down":
+#     for i in range(numeros_de_veces,-1,-1):
+#         print (i)
+# else:
+#     print("Ingrese un valor valido")
+
+#Invitados a la fiesta
+
+# gente=int(input("¿Cuanta gente acudira a la fiesta?"))
+# Max_invitados=5
+# if gente > Max_invitados:
+#     print(f"Lo siento solo,{Max_invitados} personas pueden acudir a la fiesta")
+# else:
+#     invitados= str(input("Escribe el nombre de los invitados"))
+# for i in range (invitados):
+#     print(i)
